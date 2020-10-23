43b9e9b428a6a6f9f46b8a4ab064bc6b5f3918d8
GuyARoss <guyallenross@gmail.com>
2020-10-23 01:07:25 -0400

    started the reorg

--- lib/model-router/index.ts
+++
@@ -1,4 +0,0 @@
-import modelRouter from './model-router'
-export { default as ModelTypes } from './model-types'
-
-export default modelRouter
---
+++ packages/@dour/core/index.ts
@@ -0,0 +1,6 @@
+import { testDecorator } from './lib/test-decorator'
+
+@testDecorator
+class Test {
+
+}
